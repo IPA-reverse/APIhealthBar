517f38ba11c21309bc1a093f4a5d382962f2b505
Nicolas Grenié <nicolas.grenie@gmail.com>
2015-02-18 18:53:04 +0100

    test app to send requests to API

--- api/api.js
+++ api/api.js
@@ -2,16 +2,25 @@
 var express = require('express')
 var app = express()
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(allowCrossDomain);
+
 app.get('/200',function(req,res){
-    res.send(200)
+    res.sendStatus(200)
 })
 
 app.get('/400',function(req,res){
-    res.send(400)
+    res.sendStatus(400)
 })
 
 app.get('/500',function(req,res){
-    res.send(500)
+    res.sendStatus(500)
 })
 
 app.listen(3000)
