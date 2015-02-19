32d392db0a9817e85cde85fe9d19c497a5fe6435
Nicolas Grenié <nicolas.grenie@gmail.com>
2015-02-19 11:50:54 +0100

    add port

--- api/api.js
+++ api/api.js
@@ -2,6 +2,8 @@
 var express = require('express')
 var app = express()
 
+app.set('port', process.env.PORT || 3000);
+
 var allowCrossDomain = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -23,4 +25,4 @@ app.get('/500',function(req,res){
     res.sendStatus(500)
 })
 
-app.listen(3000)
+app.listen(app.get('port'))
