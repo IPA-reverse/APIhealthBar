ee349597bc69fba596fb9ff2c16dd9abe3c7e916
dd di cesare <didi@posteo.net>
2015-02-19 17:32:45 +0100

    uuid generating function

--- ahb/methods.js
+++ ahb/methods.js
@@ -52,3 +52,12 @@ function readCookie(name, headers) {
   }
   return null;
 }
+
+function uuid() {
+  'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+  var r, v;
+  r = Math.random() * 16 | 0;
+  v = c === 'x' ? r : r & 0x3 | 0x8;
+  return v.toString(16);
+});
+}
