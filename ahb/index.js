b49723064ddac5bf2275d98cca6512d6534fd1ba
dd di cesare <didi@posteo.net>
2015-02-19 17:33:44 +0100

    push middlewware client helper

--- ahb/index.js
+++ ahb/index.js
@@ -36,6 +36,8 @@ Template.newService.events({
       "token": Session.get("XSRF-TOKEN")
     }
     Meteor.call("apitoolsCreateService", name, url, credentials, function(err, res) {
+      console.log(res)
+      Session.set("apitoolsServiceID", res["_id"]);
     })
   }
 })
@@ -43,6 +45,22 @@ Template.newService.events({
 Template.servicesList.helpers({
   "services": function() {
     return Session.get("apitoolsServicesList");
+  }
+})
+
+Template.pushMiddleware.events({
+  "click #submitMiddleware" : function() {
+    code = "return function(request, next_middleware)\n  -- every middleware has to call next_middleware,\n  -- so others have chance to process the request/response\n\n  -- deal with request\n  local response = next_middleware()\n  send.notification({msg=response.status, level='info'})\n  -- deal with response\n  return response\nend";
+    credentials = {
+      "key" : Session.get("apitoolsKey"),
+      "monitorID": Session.get("apitoolsMonitorID"),
+      "token": Session.get("XSRF-TOKEN")
+    }
+    serviceID = Session.get("apitoolsServiceID");
+
+    Meteor.call("apitoolsPushMiddleware", serviceID, code, credentials, function(err, res){
+      console.log(res);
+    })
   }
 })
 
